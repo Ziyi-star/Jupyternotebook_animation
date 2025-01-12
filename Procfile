1f822882382986aed49a8c2d2f18b01e206b419b
Ziyi Liu <78235583+Ziyi-star@users.noreply.github.com>
2025-01-12 01:29:48 +0100

    update

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: voila Mittelwertsatz.ipynb --port=$PORT --no-browser --strip_sources=True --ip=0.0.0.0
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+voila Mittelwertsatz.ipynb --port $PORT --no-browser --strip_sources=True --host=0.0.0.0
